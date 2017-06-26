64bbbbc5ad9946e5d94b8e53d912b1bab109265b
Zhuweijin87 <zhuweijin87@github.com>
2017-06-26 07:54:30 -0700

    update...

---
+++ datetime/time.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Printf("类型: %T, %s\n", now, now)
+
+	fmt.Println("time.Unix(): ", now.Unix())
+	fmt.Println("time.Local(): ", now.Local())
+	//now 是一个time.Time类型
+	fmt.Printf("time.Time:\n")
+	fmt.Printf("Year:%d, Month:%d, Day:%d\n", now.Year(), now.Month(), now.Day())
+	fmt.Printf("今天是一年中的第 %d 天\n", now.YearDay())
+	fmt.Printf("现在是: %d 时 %d 分 %d 秒\n", now.Hour(), now.Minute(), now.Second())
+	fmt.Printf("今天星期 %d\n", now.Weekday())
+	fmt.Printf("Nano秒: %d\n", now.Nanosecond())
+
+	//Date()函数
+	year, mon, day := now.Date()
+	fmt.Println(year, mon, day)	
+	hour, min, sec := now.Clock()
+	fmt.Println(hour, min, sec)
+
+	//时区
+	zone, offset := now.Zone()
+	fmt.Println("时区: ", zone, offset)
+}
