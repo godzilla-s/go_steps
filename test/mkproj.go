3d713c00dab27b5c05731c0ba862a56193e807ff
Zhuweijin87 <zhuweijin87@github.com>
2017-07-11 08:17:06 -0700

    update

--- test/mkproj.go
+++ test/mkproj.go
@@ -26,7 +26,7 @@ func getWorkDir() string {
 }
 
 func createProjDir(dir string) error {
-	err := os.Mkdir(dir, 0644)
+	err := os.Mkdir(dir, 0755)
 	if err != nil {
 		log.Fatal(err)
 		return err
@@ -34,8 +34,39 @@ func createProjDir(dir string) error {
 	return nil
 }
 
+func deleteProjDir(dir string) error {
+	return nil
+}
+
+func deleteAllProjDir(proj string) error {
+	err := os.RemoveAll(proj)
+	return err
+}
+
 func getGopathEnv() (string, error) {
 	return os.Getenv(GoPath), nil
+}
+
+func isFileExist(proj string) bool {
+	_, err := os.Stat(proj)
+	return err == nil || os.IsExist(err)
+}
+
+func newFile(filename string) bool {
+	var err error
+	var f	*os.File
+	if isFileExist(filename) {
+		f, err = os.OpenFile(filename, os.O_RDWR | os.O_APPEND, 0644)
+	} else {
+		f, err = os.Create(filename)
+	}
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.WriteString("Hello")
+	return true
 }
 
 func main() {
@@ -45,5 +76,33 @@ func main() {
 	env, _ := getGopathEnv()
 	fmt.Println(env)
 
-	os.Setenv("uuid", "12354353")
+	if isFileExist("sms77.go") {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+
+	//deleteAllProjDir("test")
+/*
+	err := createProjDir("test")
+	if err != nil {
+		log.Fatal(err)
+	}	
+
+	err = createProjDir("test/src")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = createProjDir("test/pkg")
+	if err != nil {
+        log.Fatal(err)
+    }
+
+	err = createProjDir("test/bin")
+	if err != nil {
+        log.Fatal(err)
+    }
+*/
+
 }
