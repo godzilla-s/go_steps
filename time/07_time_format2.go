33caccc0bbf5bc4dd8e0cabd02e13d824d598afa
Weijin Zhu <472214158@qq.com>
2017-08-02 09:42:38 +0800

    Create 07_time_format2.go

---
+++ time/07_time_format2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func format(level int) {
+  var format string
+  swicth level {
+    case 1:
+      format = "2006"
+     case 2:
+      format = 
+  }
+}
+
+func main() {
+  
+}
