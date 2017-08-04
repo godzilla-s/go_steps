71b9b0c367ea7a6dea6cbdaa0ef59777876386f4
Weijin Zhu <472214158@qq.com>
2017-08-04 17:36:12 +0800

    Update maps_3.go

--- base/maps_3.go
+++ base/maps_3.go
@@ -13,6 +13,22 @@ func test_nil_map(v map[string]string) {
 	}
 }
 
+func complicated_map() {
+	a := make(map[string]int)
+	
+	a["A001"] = 1
+	a["B001"] = 2
+	a["C001"] = 3
+	
+	b := make(map[string]map[string]int)
+	b["BOX_A01"] = a
+	
+	c := make(map[string]map[string]map[string]int)
+	c["HUGE_BOX_A01"] = b
+	
+	fmt.Println(c)
+}
+
 func main() {
 	test_nil_map(nil)
 }
