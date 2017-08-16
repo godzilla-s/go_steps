83e211dad2a02eec0babc0f45c7b859a4557f116
Weijin Zhu <472214158@qq.com>
2017-08-16 20:34:11 +0800

    Update 02_mysql.go
    fix little bugs

--- database/02_mysql.go
+++ database/02_mysql.go
@@ -15,18 +15,21 @@ var (
   dbpwd = "root123456"
   dbuser = "mybase"
   dbip = "127.0.0.1"
+  dbport = "3306"
 )
 
 func init() {
-  conninfo := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", dbname, dbpwd, dbip, dbuser)
+	conninfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbname, dbpwd, dbip, dbport, dbuser)
   dbconn, err := sql.Open("mysql", conninfo)
+	if err != nil {
+		log.Fatal("Open Mysql:", err)	
+	}
   dbconn.SetMaxOpenConns(1000)
-  
   // 如果保持一个空闲链接太长，则会出现出现网络连接的问题，类似下面错误： 
 	// unexpected EOF
 	// write tcp 192.168.3.90:3306: broken pipe
   // 可以试着：db.SetMaxIdleConns(0)
-  dbconn.SetMaxIldeConns(500)
+  dbconn.SetMaxIdleConns(500)
   dbconn.Ping()
   
   db = dbconn
