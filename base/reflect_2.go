7b0abada9591b97d36a029fe2031b089e32f01a3
Weijin Zhu <472214158@qq.com>
2017-08-23 19:37:26 +0800

    Update reflect_2.go

--- base/reflect_2.go
+++ base/reflect_2.go
@@ -12,12 +12,18 @@ type Data struct {
 )
 
   func attr(d1 interface{}, d2 interface{}){
+    // Value 类型
     v_d1 := reflect.ValueOf(d1)
     v_d2 := reflect.ValueOf(d2)
+    
+    fmt.Println(v_d1, v_d2)
+    
+    // Type 类型
     t_d1 := reflect.TypeOf(d1)
     t_d2 := reflect.TypeOf(d2)
     
-    fmt.Println(v_d1, v_d2)
+    fmt.Println(t_d1.String(), t_d2.String()) //该类型的值需要多少字节, ~~unsafe.Sizeof
+    
     fmt.Println(t_d1, t_d2)
     
     fmt.Println(v_d1.Kind(), v_d2.Kind())
