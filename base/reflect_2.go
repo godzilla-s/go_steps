57d4db61e270bc26733f40e544f6187cb56ed972
Weijin Zhu <472214158@qq.com>
2017-08-23 19:32:07 +0800

    Update reflect_2.go

--- base/reflect_2.go
+++ base/reflect_2.go
@@ -11,6 +11,31 @@ type Data struct {
   c   bool
 )
 
+  func attr(d1 interface{}, d2 interface{}){
+    v_d1 := reflect.ValueOf(d1)
+    v_d2 := reflect.ValueOf(d2)
+    t_d1 := reflect.TypeOf(d1)
+    t_d2 := reflect.TypeOf(d2)
+    
+    fmt.Println(v_d1, v_d2)
+    fmt.Println(t_d1, t_d2)
+    
+    fmt.Println(v_d1.Kind(), v_d2.Kind())
+    
+    
+}
 func main() {
+  d1 := Data {
+    a: 20,
+    b: "abcdefg",
+    c: true,
+  }
   
+  d2 := Data {
+    a: 100,
+    b: "ABCDEFG",
+    c: false
+  }
+  
+  attr(d1, &d2)
 }
