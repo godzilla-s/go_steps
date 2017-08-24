b1428c534083fbfabc3b7627d7c4c01fcb070d1f
Weijin Zhu <472214158@qq.com>
2017-08-24 21:48:46 +0800

    Update reflect_3.go

--- base/reflect_3.go
+++ base/reflect_3.go
@@ -5,8 +5,34 @@ import (
   "reflect"
 )
 
-func StructCopy(d1 interface{}, d2 interface{}) {
+type Data struct {
+  a     int
+  b     string
+  c     bool
+}
 
+func CopyStruct(src, dst interface{}) {
+	vsrc := reflect.ValueOf(src)
+	vdst := reflect.ValueOf(dst)
+
+	if vsrc.Kind() != reflect.Ptr || vdst.Kind() != reflect.Ptr {
+		fmt.Println("Not Struct Pointer")
+		return
+	}
+
+	numSrcFld := vsrc.NumField()
+	numDstFld := vdst.NumField()
+
+	if numSrcFld != numDstFld {
+		fmt.Println("Number of field not map")
+		return
+	}
+
+	for i:=0; i<numSrcFld; i++ {
+		if vdst.Elem().Field(i).CanSet() {
+			vdst.Elem().Field(i).Set(vsrc.Elem().Field(i))
+		}
+	}
 }
 
 func main() {
